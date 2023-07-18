c8c5bf5dea9e696467e83553190ce7c77f0b05ce
francosantana <francosantanaus@gmail.com>
2023-07-18 19:39:58 -0400

    Profile updates

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { CommonModule } from '@angular/common';
+
+import { MatIconModule } from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [CommonModule, MatIconModule, MatDividerModule]
 })
 export class ProfileComponent {
+
+
+  constructor(public authService: AuthService){
+  }
 
 }
